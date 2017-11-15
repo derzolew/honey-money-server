057c94107bde3d9ef571c6610558f891fdeb07e8
Maksim Vilcheuski <maxmaxwow@gmail.com>
2017-11-15 11:06:43 +0300

    Init project

---
+++ src/main/java/com/honeymoney/honeymoney/HoneyMoneyApplication.java
@@ -0,0 +1,12 @@
+package com.honeymoney.honeymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyMoneyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/honeymoney/honeymoney/HoneyMoneyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.honeymoney.honeymoney;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HoneyMoneyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
